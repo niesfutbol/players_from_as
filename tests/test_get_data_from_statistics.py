e6f91301ad947e35e587616b9c02406cc83201a6
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-05 22:43:30 -0700

    ✅Test `obtain_data_from_statistics()`: second example

--- tests/test_get_data_from_statistics.py
+++ tests/test_get_data_from_statistics.py
@@ -36,6 +36,41 @@ statistics = {
     "penalty": {"won": None, "commited": None, "scored": 0, "missed": 0, "saved": 0},
 }
 
+statistics_2 = {
+    "team": {
+        "id": 50,
+        "name": "Manchester City",
+        "logo": "https://media-3.api-sports.io/football/teams/50.png",
+    },
+    "league": {
+        "id": 39,
+        "name": "Premier League",
+        "country": "England",
+        "logo": "https://media-3.api-sports.io/football/leagues/39.png",
+        "flag": "https://media-2.api-sports.io/flags/gb.svg",
+        "season": 2022,
+    },
+    "games": {
+        "appearences": 23,
+        "lineups": 21,
+        "minutes": 1849,
+        "number": None,
+        "position": "Defender",
+        "rating": "7.021739",
+        "captain": False,
+    },
+    "substitutes": {"in": 2, "out": 5, "bench": 8},
+    "shots": {"total": 9, "on": 3},
+    "goals": {"total": 2, "conceded": 0, "assists": 2, "saves": None},
+    "passes": {"total": 1511, "key": 8, "accuracy": 61},
+    "tackles": {"total": 23, "blocks": 10, "interceptions": 9},
+    "duels": {"total": 104, "won": 70},
+    "dribbles": {"attempts": 7, "success": 6, "past": None},
+    "fouls": {"drawn": 8, "committed": 10},
+    "cards": {"yellow": 2, "yellowred": 0, "red": 0},
+    "penalty": {"won": None, "commited": None, "scored": 0, "missed": 0, "saved": None},
+}
+
 
 def test_obtain_data_from_statistics():
     expected = {
@@ -52,4 +87,19 @@ def test_obtain_data_from_statistics():
         "duels_won": 13,
     }
     obtained = pfa.obtain_data_from_statistics(statistics)
+    assert obtained == expected
+    expected = {
+        "minutes": 1849,
+        "shots_total": 9,
+        "shots_on": 3,
+        "passes_total": 1511,
+        "passes_key": 8,
+        "passes_accuracy": 61,
+        "tackles_total": 23,
+        "tackles_blocks": 10,
+        "tackles_interceptions": 9,
+        "duels_total": 104,
+        "duels_won": 70,
+    }
+    obtained = pfa.obtain_data_from_statistics(statistics_2)
     assert obtained == expected
