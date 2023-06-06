c9a1077ea0871ac5b3aa078309e38d8ed1304e5e
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-05 22:32:29 -0700

    ✅ Pass test

--- players_from_as/get_data_from_statistics.py
+++ players_from_as/get_data_from_statistics.py
@@ -1,2 +1,15 @@
 def obtain_data_from_statistics(statistics):
-    pass
+    expected = {
+        "minutes": statistics["games"]["minutes"],
+        "shots_total": statistics["shots"]["total"],
+        "shots_on": statistics["shots"]["on"],
+        "passes_total": statistics["passes"]["total"],
+        "passes_key": statistics["passes"]["key"],
+        "passes_accuracy": statistics["passes"]["accuracy"],
+        "tackles_total": statistics["tackles"]["total"],
+        "tackles_blocks": statistics["tackles"]["blocks"],
+        "tackles_interceptions": statistics["tackles"]["interceptions"],
+        "duels_total": statistics["duels"]["total"],
+        "duels_won": statistics["duels"]["won"],
+    }
+    return expected
