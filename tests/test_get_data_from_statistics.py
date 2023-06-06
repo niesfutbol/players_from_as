baf228ea4e71aa19019606c1aa633a22081deb81
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-05 22:50:10 -0700

    🛑🧪 Test `obtain_data_from_statistics()`: team and league

--- tests/test_get_data_from_statistics.py
+++ tests/test_get_data_from_statistics.py
@@ -85,6 +85,10 @@ def test_obtain_data_from_statistics():
         "tackles_interceptions": 1,
         "duels_total": 16,
         "duels_won": 13,
+        "team_name": "Manchester City",
+        "team_logo": "https://media-3.api-sports.io/football/teams/50.png",
+        "league_name": "Premier League",
+        "league_logo": "https://media-3.api-sports.io/football/leagues/39.png",
     }
     obtained = pfa.obtain_data_from_statistics(statistics)
     assert obtained == expected
