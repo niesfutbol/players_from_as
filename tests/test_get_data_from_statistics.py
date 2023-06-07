e073791ab682c2869d8b6608b0b444118f1354fb
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-06 19:51:35 -0700

    🎨 Format with Black

--- tests/test_get_data_from_statistics.py
+++ tests/test_get_data_from_statistics.py
@@ -218,4 +218,3 @@ def test_get_player_info_from_list():
     }
     assert obtained[626] == expected, "Assert the full player info from list"
     assert obtained[617]["minutes"] == 3150, "Assert minutos of Ederson"
-        
