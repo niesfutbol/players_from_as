396dbd49ae2be33af494ccdebadd70a8586f2404
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-07 19:41:59 -0700

    ✅ Test `get_player_and_id()`: Second example

--- tests/test_get_player_and_id.py
+++ tests/test_get_player_and_id.py
@@ -6,5 +6,5 @@ def test_get_player_and_id():
     f = open("tests/data/data_statisitcs_players_team_39_season_2022_page_2.json")
     players = json.load(f)
     obtained = pfa.get_player_and_id(players["response"])
-    assert obtained[24888] == "Hwang Hee-Chan", "Assert curp of Hwang Hee-Chan"
-
+    assert obtained[24888] == "Hwang Hee-Chan", "Assert name of Hwang Hee-Chan"
+    assert obtained[149564] == "L. Cundle", "Assert name of L. Cundle"
